8e5da96c55eb35dba09a9171aa5f3dc384a1c43e
Bruno <bruno.pejac@gmail.com>
2017-12-25 00:11:18 +0100

    door3 part2

---
+++ door03/door03.2.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+a = np.zeros([11,11], np.int)
+
+def getHood(x,y):
+    return a[x-1,y-1]+a[x,y-1]+a[x-1,y]+a[x-1,y+1]+a[x+1,y-1]+a[x+1,y]+a[x,y+1]+a[x+1,y+1]
+
+def main():
+    normal = 50
+    straight_steps = 1
+    x = 5
+    y = 5
+    a[x,y] = 1
+    while (True):
+        for i in range(0, straight_steps):
+            a[x,y] += getHood(x,y)
+            print(a[x,y])
+            x += 1
+        for i in range(0, straight_steps):
+            a[x,y] += getHood(x,y)
+            print(a[x,y])
+            y += 1
+        straight_steps += 1
+        for i in range(0, straight_steps):
+            a[x,y] += getHood(x,y)
+            print(a[x,y])
+            x -= 1
+        for i in range(0, straight_steps):
+            a[x,y] += getHood(x,y)
+            print(a[x,y])
+            y -= 1
+        straight_steps += 1
+
+main()
