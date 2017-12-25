7ef20f151a5cb15ad3f8e6f51542c410e91ca081
Bruno <bruno.pejac@gmail.com>
2017-12-25 21:22:40 +0100

    door5 part2

--- door05/door05.py
+++ door05/door05.py
@@ -1,17 +1,30 @@
 with open('data') as f:
     j = 0
-    l = f.readlines()
+    lines = f.readlines()
+    lines = list(map(lambda x: int(x), lines))
     p = 0
-    m = len(l)
-    li = []
-    for i in l:
-        li.append(int(i))
-    print(li[0])
-    while (p >= 0 and p < m):
-        t = li[p]
-        li[p] += 1
+    while (p >= 0 and p < len(lines)):
+        t = lines[p]
+        lines[p] += 1
         p += t
         j += 1
         print('.')
+
+print(j)
+
+
+with open('data') as f:
+    j = 0
+    lines = f.readlines()
+    lines = list(map(lambda x: int(x), lines))
+    p = 0
+    while (p >= 0 and p < len(lines)):
+        t = lines[p]
+        if (t >= 3):
+            lines[p] -= 1
+        else:
+            lines[p] += 1
+        p += t
+        j += 1
 
 print(j)
